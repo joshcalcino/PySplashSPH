dafe5446996fff23da2ebe6cd6e71d7f6a6d80e9
David Liptai <dliptai@swin.edu.au>
2020-07-20 10:45:31 +1000

    removed find_library() since it only returns the full path to the library on MacOS i.e. even if it finds libexact in LD_LIBRARY_PATH or similar on Unix, it will fail to load.

--- src/exact/__init__.py
+++ src/exact/__init__.py
@@ -3,34 +3,27 @@
 
 """
 
-from ctypes.util import find_library
 from ctypes import LibraryLoader, cdll
 import os
 
 
-_libexactpath = find_library("libexact")
-
-if _libexactpath is None:
+try:
+    HOME = os.environ['HOME']
+    _libexactpath = os.path.join(HOME, 'splash/build/libexact.so')
+    libexact = cdll.LoadLibrary(_libexactpath)
+except OSError:
     try:
-        HOME = os.environ['HOME']
-        _libexactpath = os.path.join(HOME, 'splash/build/libexact.so')
+        SPLASH_DIR = os.environ['SPLASH_DIR']
+        _libexactpath = os.path.join(str(SPLASH_DIR), 'build/libexact.so')
         libexact = cdll.LoadLibrary(_libexactpath)
-    except OSError:
-        try:
-            SPLASH_DIR = os.environ['SPLASH_DIR']
-            _libexactpath = os.path.join(str(SPLASH_DIR), 'build/libexact.so')
-            libexact = cdll.LoadLibrary(_libexactpath)
-        except KeyError or OSError:
-            print("PySPLASH ERROR: Could not find `libexact.so`")
-            print("Please make sure that you have SPLASH installed.")
-            print("If you do not have admin privledges, please set an")
-            print("environment variable `SPLASH_DIR` that points to the")
-            print("directory where SPLASH is located.")
-            exit(1)
+    except KeyError or OSError:
+        print("PySPLASH ERROR: Could not find `libexact.so`")
+        print("Please make sure that you have SPLASH installed.")
+        print("If you do not have admin privledges, please set an")
+        print("environment variable `SPLASH_DIR` that points to the")
+        print("directory where SPLASH is located.")
+        exit(1)
 
-else:
-    libexact = cdll.LoadLibrary(str(_libexactpath))
-    libexact.check_argcv_f()
 
 from .exact import (shock, shock_sr, sedov, polytrope,
                     toystar1D, toystar2D, gresho, rhoh,
